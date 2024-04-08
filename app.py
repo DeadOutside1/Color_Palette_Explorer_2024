112687a2567b274aae3f410c49774c277661bb1c
Amir <210103034@stu.sdu.edu.kz>
2024-04-09 03:14:48 +0500

    just check for correctly work git

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def index():
 def about():
     return render_template("about.html")
 
+def justtest():
+    pass
+
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return "User page" + name + "-" + str(id)
