9ef241cff0973c8067b7d7e3173290e2fd2369e4
Amir <210103034@stu.sdu.edu.kz>
2024-04-09 18:12:02 +0500

    1) add sign in , sign up html css structure 2)add color palete html structure with css

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-app =  Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
 
 @app.route('/')
 def index():
--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,10 @@
 const loginBtn = document.getElementById('loginBtn');
 const signupLink = document.getElementById('signupLink');
 const loginPopup = document.getElementById('loginPopup');
+const SignUpBtn = document.getElementById('SignUpBtn');
+const signupPopup = document.getElementById('signupPopup');
 const overlay = document.getElementById('overlay');
+
 
 //open popup
 function openPopup() {
@@ -15,8 +18,21 @@ function closePopup() {
     overlay.style.display = 'none';
 }
 
+function openPopup1() {
+    signupPopup.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+// Function to close popup
+function closePopup1() {
+    signupPopup.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
 //click login button
 loginBtn.addEventListener('click', openPopup);
+//click sign up button
+SignUpBtn.addEventListener('click', openPopup1);
 
 // click signup link button
 signupLink.addEventListener('click', openPopup);
